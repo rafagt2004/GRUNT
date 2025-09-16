265ee775e439022af66944f558f8701871ef6f69
rafaerion <rafagtsouza.jr@gmail.com>
2025-09-16 10:36:13 -0300

    Initialize package.json with basic configuration and development dependencies for Grunt

---
+++ Gruntfile2.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    "main.css": "main.less"
+                }
+            },
+            production: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    "main.min.css": "main.less"
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    implementation: require('sass'),
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css' : 'main.scss'
+                }
+            }
+        }
+    })
+
+    grunt.registerTask('olaGrunt', function() {
+        const done = this.async();
+        setTimeout(function() {
+            console.log('Ola Grunt!')
+            done();
+        }, 3000)
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-sass');
+
+    grunt.registerTask('default', ['less', 'sass'])
+}
