0ba39cab16b780cdfdeb4fba506678ea30ed139c
rafael <rafagtsouza.jr@gmail.com>
2025-09-16 21:19:01 -0300

    feat: add initial HTML structure and styles for the random number generator
    - Created index.html with a form for user input and basic layout.
    - Added main.less for styling, including variables for colors and fonts.
    - Updated package.json to include necessary Grunt plugins for building and cleaning.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
+const { file } = require("grunt");
+
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         less: {
             development: {
                 files: {
-                    "main.css": "main.less"
+                    "dev/styles/main.css": "src/styles/main.less"
                 }
             },
             production: {
@@ -12,36 +14,78 @@ module.exports = function(grunt) {
                     compress: true
                 },
                 files: {
-                    "main.min.css": "main.less"
+                    "dist/styles/main.min.css": "src/styles/main.less"
                 }
             }
         },
-        sass: {
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            },
+            html: {
+                files : ['src/index.html'],
+                tasks : ['replace:dev']
+            }
+        },
+        replace : {
+            dev: {
+                options : {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files : [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options : {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files : [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
             dist: {
                 options: {
-                    style: 'compressed'
+                    removeComments: true,
+                    collapseWhitespace: true,
                 },
-                files: {
-                    'main2.css' : 'main.scss'
+                files : {
+                    'prebuild/index.html' : 'src/index.html'
                 }
             }
         },
-        concurrent: {
-            target: ['olaGrunt', 'less']
-        }
-    })
-
-    grunt.registerTask('olaGrunt', function() {
-        const done = this.async();
-        setTimeout(function() {
-            console.log('Ola Grunt!')
-            done();
-        }, 3000)
+        clean : ['prebuild']
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', ['concurrent'])
+    grunt.registerTask('default', ['watch'])
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist','clean'])
 }
